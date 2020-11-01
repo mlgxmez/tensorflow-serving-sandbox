69178c6f17a14713b1b14d8d4f989b10fb724709
mlgxmez <mlgxmez@gmail.com>
2020-11-01 12:23:19 +0100

    Set flake8 format

--- models/new_models.py
+++ models/new_models.py
@@ -4,15 +4,22 @@ import os
 
 import tensorflow as tf
 import tensorflow_hub as hub
+from tensorflow.keras.layers.experimental.preprocessing import Resizing
 
+HUB_URL = "https://tfhub.dev/tensorflow"
+MODEL_NAME = "efficientnet"
 
 _MODEL_SIZES_ = {
-    "small": ("https://tfhub.dev/tensorflow/efficientnet/b0/classification/1", 224), 
-    "medium": ("https://tfhub.dev/tensorflow/efficientnet/b3/classification/1", 300),
-    "big": ("https://tfhub.dev/tensorflow/efficientnet/b3/classification/1", 528)
+    "small": (os.path.join(HUB_URL, MODEL_NAME,
+                           "b0", "classification", "1"), 224),
+    "medium": (os.path.join(HUB_URL, MODEL_NAME,
+                            "b3", "classification", "1"), 300),
+    "big": (os.path.join(HUB_URL, MODEL_NAME,
+                         "b5", "classification", "1"), 528)
     }
 
 FILE_LABELS = "https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt"
+
 
 class ImportModelHub(tf.keras.Model):
     """
@@ -38,32 +45,49 @@ class ImportModelHub(tf.keras.Model):
         def _read_image(img_file):
             img_bytes = tf.reshape(img_file, [])
             img = tf.io.decode_jpeg(img_bytes, channels=3)
-            img = tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis,...]
+            img = tf.image.convert_image_dtype(img, tf.float32)
+            img = img[tf.newaxis, ...]
             return img
 
+        def id_image_prediction(x):
+            """
+            Return the label with highest logits
+            """
+            return self.labels[tf.argmax(x)]
+
         x = _read_image(x)
-        rescaled_x = tf.keras.layers.experimental.preprocessing.Resizing(self.size, self.size)(x)
+        rescaled_x = Resizing(self.size, self.size)(x)
         logits = self.model(rescaled_x)
-        id_image_prediction = lambda x: self.labels[tf.argmax(x)]
         id_to_text = tf.map_fn(id_image_prediction, logits, tf.string)
         return id_to_text
 
-            
+
 def run(model_size, output_dir):
-    output_dir_ = os.path.join(output_dir, "efficientnet") 
+    output_dir_ = os.path.join(output_dir, MODEL_NAME)
     if not os.path.isdir(output_dir_):
-        os.makedirs(output_dir_,exist_ok=True)
+        os.makedirs(output_dir_, exist_ok=True)
     model_versions = os.listdir(output_dir_)
-    last_version = max([int(v) for v in model_versions]) if len(model_versions) > 0 else 0 
+    last_version = 0
+    if len(model_versions) > 0:
+        last_version = max([int(v) for v in model_versions])
     model = ImportModelHub(model_size)
     # Set incremental values of folders
     last_version += 1
-    tf.saved_model.save(model, os.path.join(output_dir, "efficientnet", str(last_version)))
+    tf.saved_model.save(model, os.path.join(output_dir,
+                                            MODEL_NAME,
+                                            str(last_version)
+                                            ))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--size', choices=list(_MODEL_SIZES_.keys()), help="Choose the size of models to be saved")
-    parser.add_argument('--output_dir', type=str, default="./models", help="Output path where models are saved")
+    parser.add_argument('--size',
+                        choices=list(_MODEL_SIZES_.keys()),
+                        help="Choose the size of models to be saved")
+    parser.add_argument('--output_dir',
+                        type=str,
+                        default="./models",
+                        help="Output path where models are saved")
     args = parser.parse_args()
 
     # Run the script with proper arguments
